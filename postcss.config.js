2ea5e077b4ab85778e46a4b0f47b5320c430f680
buzz <buzz@users.noreply.github.com>
2021-07-12 03:29:03 +0200

    Update

--- postcss.config.js
+++ postcss.config.js
@@ -2,9 +2,18 @@ const postcssImport = require("postcss-import");
 const precss = require("precss");
 const autoprefixer = require("autoprefixer");
 const postcssCustomMedia = require("postcss-custom-media");
+const postcssCustomProperties = require("postcss-custom-properties");
+const postcssColorMod = require("postcss-color-mod-function");
 
 module.exports = {
   parser: "sugarss",
   map: false,
-  plugins: [postcssImport(), precss(), postcssCustomMedia(), autoprefixer()],
+  plugins: [
+    postcssImport(),
+    precss(),
+    postcssCustomMedia(),
+    // postcssCustomProperties(),
+    postcssColorMod(),
+    autoprefixer(),
+  ],
 };
